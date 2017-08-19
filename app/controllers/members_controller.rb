52bbda6256fb3cf3caf2ff8878ab89fd8c5e83c0
Sostenes Pereira <apenassostenes@gmail.com>
2017-08-19 19:17:01 -0300

    Add search app

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,7 +4,8 @@ class MembersController < ApplicationController
   # GET /members
   # GET /members.json
   def index
-    @members = Member.all.paginate(:page => params[:page], :per_page => 10)
+    @q = Member.ransack(params[:q])
+    @members = @q.result(distinct: true).paginate(:page => params[:page], :per_page => 10)
   end
 
   # GET /members/1
