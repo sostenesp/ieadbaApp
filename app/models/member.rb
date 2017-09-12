a93fb9a36b9c8c7d1ee42305ed0679da347d6e59
Sostenes Pereira <apenassostenes@gmail.com>
2017-09-11 23:33:48 -0300

    Refactoring action show photo missing App

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ApplicationRecord
-  has_attached_file :avatar, styles: { normal: "130x130", photo: "90x120", thumb: "80x80>", mini: "23x23" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { normal: "130x130", photo: "90x120", thumb: "80x80>", mini: "23x23" }, default_url: "/images/thumb/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates :name, :birth_date, :address, :marital_status, :sex, :cpf, :rg, :member, :congregated, :role, :ebd, :baptized, :father_name, :mother_name, :congregation, :dizymist, :affiliated_missions, presence: true 
 end
